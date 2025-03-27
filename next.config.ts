0b248561a9c917a72179854f3a057694bdb6b570
Holandara <saraholandatavares@gmail.com>
2025-03-27 15:16:00 -0300

    Build 1

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,26 @@
 // next.config.ts
 import type { NextConfig } from 'next';
+import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
   images: {
     domains: [],
   },
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+  webpack: (config: Configuration, { isServer }: { isServer: boolean }) => {
     if (!isServer) {
-      config.resolve.fallback = { 
-        ...config.resolve.fallback,
-        fs: false 
+      config.resolve = config.resolve || {}; // Garante que resolve existe
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}), // Evita erro caso fallback não exista
+        fs: false
       };
     }
     
+    config.module = config.module || {}; // Garante que module existe
+    config.module.rules = config.module.rules || []; // Garante que rules existe
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
 
     return config;
   }
