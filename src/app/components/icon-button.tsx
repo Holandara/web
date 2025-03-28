814961182f8f9924917aa865471fefd940aa1b14
Holandara <saraholandatavares@gmail.com>
2025-03-28 10:51:48 -0300

    Rep3

--- src/app/components/icon-button.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode} from "react"
-
-interface IconButtonProps{}
-
-export function IconButton(props: IconButtonProps){
-    return <button className="-mr-2 p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900" {...props}/>
-        
-}
